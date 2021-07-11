6d0072c525b2c8020da7d18f7326ba2778f2e741
Wahiduzzaman Siddique <wahid2272@gmail.com>
2021-07-12 00:04:24 +0300

    materialize css set up for the project

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
+import React, {useEffect} from 'react'
+import 'materialize-css/dist/css/materialize.min.css'
+import M from 'materialize-css/dist/js/materialize.min.js'
 import './App.css';
 
 function App() {
+  useEffect(()=> {
+    // Init materialize JS
+    M.AutoInit();
+  })
   return (
     <div className="App">
       Hello

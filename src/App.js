cfc8f4ec3c2a039c57754c319e461f8e0ad510e4
Wahiduzzaman Siddique <wahid2272@gmail.com>
2021-07-15 22:35:57 +0300

    addbutton component created and added to app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {useEffect} from 'react'
 import SearchBar from './components/layout/SearchBar';
 import Logs from './components/logs/Logs';
+import AddButton from './components/layout/AddButton';
 import 'materialize-css/dist/css/materialize.min.css'
 import M from 'materialize-css/dist/js/materialize.min.js'
 import './App.css';
@@ -14,6 +15,7 @@ function App() {
     <>
       <SearchBar/>
       <div className="container">
+        <AddButton/>
         <Logs/>
       </div>
     </>

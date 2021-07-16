3ef5314781df48d1b931f59cbd83088174cec453
Wahiduzzaman Siddique <wahid2272@gmail.com>
2021-07-16 22:47:14 +0300

    addlogmodal component redesigned and reminder message added in empty onClick

--- src/components/logs/AddLogModal.js
+++ src/components/logs/AddLogModal.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import M from 'materialize-css/dist/js/materialize.min.js';
 
 const AddLogModal = () => {
     const [message, setMessage] = useState('');
@@ -11,7 +12,12 @@ const AddLogModal = () => {
     }
 
     const onSubmit = () => {
-        console.log(message, tech, attention);
+        if(message === '' || tech === '') {
+            M.toast({ html: 'Please enter a message and tech' })
+        }
+        else {
+            console.log(message, tech, attention);
+        }
     }
 
     return (
@@ -51,7 +57,7 @@ const AddLogModal = () => {
                 </div>
             </div>
             <div className="modal-footer">
-                <a href="#!" onClick={onSubmit} className='modaal-close waves-effect waves-green btn-flat'>Enter</a>
+                <a href="#!" onClick={onSubmit} className='modaal-close waves-effect blue waves-light btn'>Enter</a>
             </div>
         </div>
     );

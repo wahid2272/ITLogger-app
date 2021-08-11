e52e33e20475fe53ef7c9a84dff23800337a8ecf
Wahiduzzaman Siddique <wahid2272@gmail.com>
2021-08-11 23:58:11 +0300

    logReducer created and added to root reducer

---
+++ src/reducers/logReducer.js
@@ -0,0 +1,13 @@
+const intialState = {
+  logs: null,
+  current: null,
+  loading: false,
+  error: null
+}
+
+export default (state = intialState, action) => {
+  switch(action.type) {
+    default:
+      return state;
+  }
+} 

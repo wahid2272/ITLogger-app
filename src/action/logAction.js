460c68400e64395d7056c79bd14aed67dca32ae5
Wahiduzzaman Siddique <wahid2272@gmail.com>
2021-08-19 23:53:54 +0300

    getLogs function of logAction component restructured

--- src/action/logAction.js
+++ src/action/logAction.js
@@ -1,17 +1,23 @@
 import { GET_LOGS, SET_LOADING, LOGS_ERROR } from "./types";
 
-export const getLogs = () => {
-    return async (dispatch) => {
+export const getLogs = () => async dispatch => {
+    try {
         setLoading();
-
+    
         const res = await fetch('/logs');
         const data = await res.json();
-
+    
         dispatch({
             type: GET_LOGS,
             payload: data
         })
-    } 
+    }
+    catch (err) {
+        dispatch({
+            type: LOGS_ERROR,
+            payload: err.response.data
+        })
+    }
 }
 
 // set loading to true
